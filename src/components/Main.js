19974ca3e26f403691d24ca9d8be9817c2b81405
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-06-16 09:57:08 -0600

    add creat and route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,16 @@
 import { Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import Home from "../pages/Home"
+import Home from "../pages/Home";
+import Create from "../pages/Create";
 
 const Main = (props) => {
     return (
         <div>
             <Route exact path='/'>
                 <Home />
+            </Route>
+            <Route  path='/create'>
+                <Create />
             </Route>
            
            
