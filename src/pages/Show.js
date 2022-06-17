d152761351d067e8f332becce6faa65a6f325bb7
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-06-17 11:56:13 -0600

    wip add show route

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,13 @@
+
 const Show = (props) => {
+const id = props.match.params.id;
+const card = props.cards.find(p => p._id === id)
     return (
         <div>
-            <h1> Individual Card</h1>
+            <h3> {card.language}</h3>
+            <h3> {card.mater}</h3>
+            <h3> {card.translation}</h3>
+            <h3> {card.image}</h3>
         </div>
     )
 }
