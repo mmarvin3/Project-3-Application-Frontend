9f78cec4234ae7d9d923ecca01fc47bbdf5bbb9d
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-06-23 12:37:30 -0600

    add login info

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+
+const Login = (props) => {
+    return (
+        <div className="loginDiv">
+            <h1>Under Construction</h1>
+            <img className="loginImg" src="https://i.imgur.com/unMSTCA.jpg" />
+        </div>
+    )
+}
+
+export default Login
