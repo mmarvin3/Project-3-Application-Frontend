628b402f9b7d7190b989554e644632a3296ff52c
Essence Fuller <105015686+eufuller@users.noreply.github.com>
2022-06-22 17:08:52 -0700

    Toggle effect for form and flashcard display

--- src/pages/Italian.js
+++ src/pages/Italian.js
@@ -22,21 +22,41 @@ const Italian = (props) => {
 
     const loaded = () => {
         return props.italian.map(card => (
+            showCards?
             <li key={card._id} className="card">
                 <Link to={`/show/${card._id}`}>
                     <h1>{card.mater}</h1>
                 </Link>
             </li>
-        ));
+             :null
+             
+        )); 
     }
 
     const loading = () => {
         return <h1>Loading...</h1>
     }
 
+    const [showCards, setShowCards] = useState(true)
+    const [showForm, setShowForm] = useState(false)
+    const [showCreateButton, setShowCreateButton] = useState(true)
+   
+    function submitAction(){
+        setShowCards(true);
+        setShowForm(false);
+        setShowCreateButton(true);
+    }
+   
+    function createAction(){
+        setShowCards(false);
+        setShowForm(true);
+        setShowCreateButton(false);
+    }
+
     return (
         <section>
-            <form className="createForm" onSubmit={handleSubmit}>
+            {showForm ?
+            <form className="createForm" onClick={handleSubmit}>
                 <h2>Mater</h2>
                 <input
                     value={newForm.mater}
@@ -58,10 +78,14 @@ const Italian = (props) => {
                     name="image"
                     type="text"
                 />
-                <input
-                    className="create" type="submit" value="Create Card" />
+                <button
+                    className="create" onClick={() => submitAction()}>Submit</button>
             </form>
+            :null}
             {props.italian ? <ol className="cardDeck" style={{ textAlign: "left" }}>{loaded()}</ol> : <ol>{loading()}</ol>}
+            {showCreateButton ?
+            <button className="create" onClick={()=>createAction()}>Create New Card</button>
+            :null} 
         </section>
     )
 }
